ff9f0ac09f9afbef73d7df9f36b0fd4b6890a876
alpha0212 <qkrcjfgml321@naver.com>
2022-09-28 12:53:14 +0900

    Change: App version [#0.1]

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Navbar, Review, Footer, Intro } from "src/components";
+import { Navbar, Intro, SnackBar } from "src/components";
 
 import * as S from "./styled";
 
-export const MainPage: React.FC = () => (
-  <>
-    <Navbar />
-    <Intro />
-    <S.Line>📌 부모님들의 솔직 리뷰</S.Line>
-    <Review />
-    <Footer />
-  </>
-);
+export const MainPage: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Intro />
+      <SnackBar />
+    </>
+  );
+};
