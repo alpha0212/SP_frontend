e2350de0a35e1c75375f67f15cb3243ffd10bb7c
alpha0212 <qkrcjfgml321@naver.com>
2022-09-30 00:39:54 +0900

    Feat: Login & Logout Box

--- src/components/common/Intro/index.jsx
+++ src/components/common/Intro/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { host } from "src/LinkHost";
+import { useMediaQuery } from "react-responsive";
 
 import * as S from "./styled";
 
 export const Intro = () => {
+  const isDeskTop = useMediaQuery({ minWidth: "50rem" });
   const [authState, setAuthState] = useState({
     id: 0,
     user_id: "",
+    user_name: "",
     status: false,
   });
 
@@ -16,7 +20,7 @@ export const Intro = () => {
   });
   useEffect(() => {
     axios
-      .get("http://localhost:8080/auth/auth", {
+      .get(`http://${host}:8080/auth/auth`, {
         headers: {
           accessToken: localStorage.getItem("accessToken"),
         },
@@ -29,6 +33,7 @@ export const Intro = () => {
           setAuthState({
             id: res.data.id,
             user_id: res.data.user_id,
+            user_name: res.data.user_name,
             status: true,
           });
           setTeaState({
@@ -37,21 +42,54 @@ export const Intro = () => {
           });
         }
       });
-  }, [authState]);
+  }, []);
   /**
    * authState를 디펜던시에 넣어 상태가 업데이트 될 때 한번 더 사용하여
    * "로그인해야 이용가능한 서비스입니다!" 라는 문장을 로그인 상태에 따라
    * 바꿔서 보여줄 수 있다.
    */
+  const FuncLogout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({
+      user_id: "",
+      user_name: "",
+      status: false,
+    });
+  };
   return (
     <S.Background>
+      {!authState.status ? (
+        <div style={{ textAlign: "center", marginBottom: "5rem" }}>
+          로그인해야 이용가능한 서비스입니다!
+        </div>
+      ) : (
+        <></>
+      )}
       <S.MainContainer>
         {!authState.status ? (
-          <div style={{ textAlign: "center" }}>
-            로그인해야 이용가능한 서비스입니다!
-          </div>
+          <S.AuthContainer>
+            <S.Flex>
+              <S.auth>
+                <S.AuthTitleLink to="/auth/login">로그인</S.AuthTitleLink>
+              </S.auth>
+              <S.auth>
+                <S.AuthTitleLink to="/auth/register">회원가입</S.AuthTitleLink>
+              </S.auth>
+            </S.Flex>
+          </S.AuthContainer>
         ) : (
-          <></>
+          <S.AuthContainer>
+            <S.Flex>
+              <S.auth>
+                <S.AuthTitle style={{ marginTop: "0.2rem" }}>
+                  {authState.user_name}
+                </S.AuthTitle>
+              </S.auth>
+              <S.Logout onClick={FuncLogout}>
+                <S.AuthTitle>로그아웃</S.AuthTitle>
+              </S.Logout>
+            </S.Flex>
+          </S.AuthContainer>
         )}
         <S.SectionContainer>
           <S.SectionOne>
@@ -80,10 +118,17 @@ export const Intro = () => {
               )}
 
               <S.explainContainer>
-                <S.explainBox>
-                  날짜별로 자신이 공부한 시간과
-                  <br /> 총 공부량, 하루 평균 공부량을 <br />볼 수 있습니다.
-                </S.explainBox>
+                {isDeskTop ? (
+                  <S.explainBox>
+                    날짜별로 자신이 공부한 시간과
+                    <br /> 총 공부량, 하루 평균 공부량을 볼 수 있습니다.
+                  </S.explainBox>
+                ) : (
+                  <S.explainBox>
+                    날짜별로 자신이 공부한 시간과
+                    <br /> 총 공부량, 하루 평균 공부량을 <br />볼 수 있습니다.
+                  </S.explainBox>
+                )}
               </S.explainContainer>
             </S.TitleContainer>
           </S.SectionTwo>
