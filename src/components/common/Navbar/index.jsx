a5e1f6539599124730cdee1afcc7e08425cc407c
alpha0212 <qkrcjfgml321@naver.com>
2022-09-30 00:41:36 +0900

    Fix: Empty dependency

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,17 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "src/helper/AuthContext";
 
 import * as S from "./styled";
 
 export const Navbar = () => {
-  const [authState, setAuthState] = useState({
-    user_name: "",
-    id: "",
-    status: false,
-  });
-
   const [teaState, setTeaState] = useState({
     t_job: "",
     status: false,
@@ -26,14 +18,8 @@ export const Navbar = () => {
       })
       .then((response) => {
         if (response.data.error) {
-          setAuthState({ ...authState, status: false });
           setTeaState({ ...teaState, status: false });
         } else {
-          setAuthState({
-            user_name: response.data.user_name,
-            id: response.data.id,
-            status: true,
-          });
           setTeaState({
             t_job: response.data.t_job,
             status: true,
@@ -44,7 +30,6 @@ export const Navbar = () => {
 
   const logout = () => {
     localStorage.removeItem("accessToken");
-    setAuthState({ user_name: "", status: false });
     setTeaState({ t_job: "", status: false });
   };
 
@@ -52,37 +37,14 @@ export const Navbar = () => {
     <>
       <S.NavbarContainer>
         <S.NavbarFormGroup>
-          {!authState.status ? (
-            <>
-              <S.NavbarA to="auth/login">로그인&nbsp;|</S.NavbarA>
-              <S.NavbarA to="auth/register">&nbsp;회원가입</S.NavbarA>
-            </>
+          <S.Logo>Skyplanner</S.Logo>
+          {teaState.t_job === "teacher" ? (
+            <S.NavbarA to="st/list">
+              <S.TextSet>학생관리</S.TextSet>
+            </S.NavbarA>
           ) : (
-            <>
-              {teaState.t_job === "teacher" ? (
-                <></>
-              ) : (
-                <div
-                  style={{
-                    color: "black",
-                    paddingTop: "0.5rem",
-                    fontSize: "2rem",
-                    marginRight: "0.3rem",
-                  }}
-                >
-                  {authState.user_name}
-                </div>
-              )}
-              {teaState.t_job === "teacher" ? (
-                <S.NavbarA to="st/list">
-                  <S.TextSet>학생관리</S.TextSet>
-                </S.NavbarA>
-              ) : (
-                <></>
-              )}
-            </>
+            <></>
           )}
-          {authState.status && <S.Logout onClick={logout}>로그아웃</S.Logout>}
         </S.NavbarFormGroup>
       </S.NavbarContainer>
     </>
