8a6f07a821312941a43cc6d14cc0af744b15765c
alpha0212 <qkrcjfgml321@naver.com>
2022-10-31 21:59:40 +0900

    cspell.json

--- src/components/common/Students/StMenu/index.tsx
+++ src/components/common/Students/StMenu/index.tsx
@@ -1,20 +1,8 @@
 import React from "react";
 import * as S from "./styled";
 import { useNavigate } from "react-router";
-import { StList } from "../StList";
 
 export const StMenu = () => {
   const navigate = useNavigate();
-  return (
-    <S.Background>
-      <S.MainContainer>
-        <S.SectionContainer>
-          <S.TimeInfo>학생 시간데이터</S.TimeInfo>
-        </S.SectionContainer>
-        <S.SectionContainer2>
-          <S.Profile>학생 프로필</S.Profile>
-        </S.SectionContainer2>
-      </S.MainContainer>
-    </S.Background>
-  );
+  return <S.Background></S.Background>;
 };
