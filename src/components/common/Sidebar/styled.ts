690be9f3a875be1884e19b1a82bcbca498f93de7
alpha0212 <qkrcjfgml321@naver.com>
2022-11-01 00:22:35 +0900

    Sidebar Fix & Refer Blog

--- src/components/common/Sidebar/styled.ts
+++ src/components/common/Sidebar/styled.ts
@@ -1,7 +1,17 @@
 import styled from "@emotion/styled";
 
 export const SideContainer = styled.div<{ testProps: boolean }>`
-  width: 3rem;
-  padding-top: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  transition: 0.4s ease;
+  width: 20%;
+  height: 100%;
+  z-index: 99;
   background-color: ${(props) => (props.testProps ? "black" : "blue")};
+  color: white;
+  font-size: 3rem;
 `;
+
+//https://ji-u.tistory.com/22
