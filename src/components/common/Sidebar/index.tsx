8a1067996ee9857df112b5c87c3192d5e347d142
alpha0212 <qkrcjfgml321@naver.com>
2022-11-01 00:21:35 +0900

    Props to emtion styled

--- src/components/common/Sidebar/index.tsx
+++ src/components/common/Sidebar/index.tsx
@@ -3,5 +3,5 @@ import * as S from "./styled";
 import { SideContainer } from "./styled";
 export const Sidebar: React.FC = () => {
   const [bool, setBool] = useState(true);
-  return <SideContainer testProps={bool}>asd</SideContainer>;
+  return <SideContainer testProps={bool}>S</SideContainer>;
 };
