736a8f6084d98d2d155d84f183119ebea7dc3bca
alpha0212 <qkrcjfgml321@naver.com>
2022-11-01 11:22:31 +0900

    Feature: sexy width sidebar

--- src/components/common/Sidebar/index.tsx
+++ src/components/common/Sidebar/index.tsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import * as S from "./styled";
 import { SideContainer } from "./styled";
 export const Sidebar: React.FC = () => {
-  const [bool, setBool] = useState(true);
-  return <SideContainer testProps={bool}>asd</SideContainer>;
+  const [bool, setBool] = useState(false);
+  const isOpen = () => {
+    setBool(!bool);
+  };
+  return (
+    <SideContainer testProps={bool}>
+      <button onClick={isOpen}>LOGO</button>
+      {bool ? <div>메뉴</div> : <div></div>}
+    </SideContainer>
+  );
 };
--- src/components/common/Sidebar/styled.ts
+++ src/components/common/Sidebar/styled.ts
@@ -1,7 +1,15 @@
 import styled from "@emotion/styled";
 
 export const SideContainer = styled.div<{ testProps: boolean }>`
-  width: 3rem;
-  padding-top: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: ${(props) => (props.testProps ? "18%" : "9%")};
+  height: 100%;
+  z-index: 99;
   background-color: ${(props) => (props.testProps ? "black" : "blue")};
+  color: white;
+  transition: 0.4s ease;
+  font-size: 3rem;
 `;
