fba5bc64a4bd0a4a6d61aa9eda58d30e01b4f3df
alpha0212 <qkrcjfgml321@naver.com>
2022-11-02 12:57:06 +0900

    Delete: Sidebar, MainSection

--- src/components/common/Managements/MainSection/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { StList } from "../../Students";
-import * as S from "./styled";
-export interface Props {
-  props1: boolean;
-}
-export const MainSection: React.FC<Props> = (props1) => {
-  return !props1 ? <></> : <StList />;
-};
